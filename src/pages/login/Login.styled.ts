92d237fd3f00d3c581c3400c62cca478c2653dc7
jigomgom <107230384+jigomgom@users.noreply.github.com>
2024-06-23 12:18:11 +0900

    Feat/#2 샘플코드 작성 로그인 (#7)
    * feat: react-hook-form 설치(#1)
    
    * feat: ts 설정 및 svg declare 설정(#1)
    
    * feat: emotion 관련 package.json(#2)
    
    * feat: login 기본 UI 만들기(#2)
    
    * feat: Login 로직을 state로 관리하기(#2)
    
    * feat: Login 로직을 hook-form로 관리하기(#2)
    
    * feat: Login 로직을 hook-form로예제추가(#2)

---
+++ src/pages/login/Login.styled.ts
@@ -0,0 +1,44 @@
+import styled from "@emotion/styled";
+import { css } from "@emotion/react";
+
+export const Section = styled.section`
+  width: 600px;
+  height: 500px;
+  margin: 20px auto;
+`;
+
+export const LoginForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 8px;
+
+  padding: 15px;
+
+  border: 1px solid gray;
+`;
+
+export const Ptag = styled.p`
+  height: 10px;
+  font-size: 1.4rem;
+  color: black;
+`;
+
+export const FormContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 500px;
+`;
+
+export const Input = styled.input<{ hasError: boolean }>`
+  ${({ hasError }) => css`
+    padding: 5px;
+    font-size: 1.4rem;
+    border: 1px solid ${hasError ? "red" : "black"};
+  `}
+`;
+
+export const ErrorMessage = styled.p`
+  color: red;
+`;
