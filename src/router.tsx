f3e66b2c945f67d9222c510035469def2d6fa71b
jigomgom <dydrkfl5519@gmail.com>
2024-06-23 12:51:23 +0900

    feat: Router의 path 연결(#3)

--- src/router.tsx
+++ src/router.tsx
@@ -6,6 +6,7 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
+import { PATH } from "constants/index";
 import * as P from "pages";
 
 interface RouterProps {
@@ -14,7 +15,19 @@ interface RouterProps {
 
 const Router = ({ children }: RouterProps) => {
   const router = createBrowserRouter(
-    createRoutesFromElements(<Route element={children}></Route>)
+    createRoutesFromElements(
+      <Route element={children}>
+        <Route path={PATH.ROOT} element={<P.Root />} />
+        <Route
+          path={`${PATH.EXAMPLE}/${PATH.LOGIN}`}
+          element={<P.StateLogin />}
+        />
+        <Route
+          path={`${PATH.EXAMPLE}/${PATH.HOOK_FORM_LOGIN}`}
+          element={<P.StateLogin />}
+        />
+      </Route>
+    )
   );
 
   return <RouterProvider router={router} />;
